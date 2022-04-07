135acb2c806ffe0c9aa77bcf698cc8d36d3b52dc
dadabricks <55216120+dadabricks@users.noreply.github.com>
2022-04-07 16:58:58 -0700

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -1,6 +1,8 @@
 # Databricks notebook source
 # MAGIC %pip install git+https://github.com/mlflow/mlflow@master
-# MAGIC 
-# MAGIC import mlflow
-# MAGIC run = mlflow.start_run()
-# MAGIC mlflow.end_run()
+
+# COMMAND ----------
+
+import mlflow
+run = mlflow.start_run()
+mlflow.end_run()
