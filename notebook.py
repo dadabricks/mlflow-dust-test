fd1f392ee7c1be070cc6790c5df347020ecad3c5
dadabricks <55216120+dadabricks@users.noreply.github.com>
2022-04-07 16:58:09 -0700

    Create notebook.py

---
+++ notebook.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+# MAGIC %pip install git+https://github.com/mlflow/mlflow@master
+# MAGIC 
+# MAGIC import mlflow
+# MAGIC run = mlflow.start_run()
+# MAGIC mlflow.end_run()
