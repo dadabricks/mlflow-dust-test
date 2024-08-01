0889dd0b74573d8e66f11147bedc15f366cd5fb2
dadabricks <55216120+dadabricks@users.noreply.github.com>
2024-08-01 11:15:36 -0700

    Create test-notebook-runid.py

---
+++ test-notebook-runid.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# MAGIC %pip install mlflow
+
+# COMMAND ----------
+
+
+dbutils.library.restartPython()
+
+import mlflow
+
+run = mlflow.start_run()
+run_id = mlflow.active_run().info.run_id
+mlflow.end_run()
+dbutils.notebook.exit(run_id)
